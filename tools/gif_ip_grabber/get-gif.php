d946f58265dbff107fd6dfc47b7ff7ea2f02ce25
Lord of the IoT <lord_of_the_iot@proton.me>
2023-07-05 18:55:10 +0100

    Delete tools directory

--- tools/gif_ip_grabber/get-gif.php
+++
@@ -1,4 +0,0 @@
-$line = date('Y-m-d H:i:s') . " - $_SERVER[REMOTE_ADDR]";
-file_put_contents('visitors.log', $line . PHP_EOL, FILE_APPEND);
-header("Location: https://lord-of-the-iot.github.io/tools/gif_ip_grabber/hello.gif"); 
-exit();
